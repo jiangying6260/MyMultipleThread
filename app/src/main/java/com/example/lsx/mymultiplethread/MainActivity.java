1187ab049907eabf92dfedfc67995e901df381c4
jy <jy@163.com>
2016-07-31 16:51:30 +0800

    加了进度条ProgressBar

--- app/src/main/java/com/example/lsx/mymultiplethread/MainActivity.java
+++ app/src/main/java/com/example/lsx/mymultiplethread/MainActivity.java
@@ -8,12 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView mImageView;
     private Button mLoadImageButton;
     private Button mToastButton;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         mImageView= (ImageView) findViewById(R.id.activity_main_image_view);
         mLoadImageButton= (Button) findViewById(R.id.activity_main_imageview_button);
         mToastButton= (Button) findViewById(R.id.activity_main_toast_button);
+        mProgressBar= (ProgressBar) findViewById(R.id.activity_main_progress_bar);
 
         mLoadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,22 +42,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    class LoadImageTask extends AsyncTask<Void,Void,Bitmap>{
+    class LoadImageTask extends AsyncTask<Void,Integer,Bitmap>{
+
+        @Override
+        protected void onPreExecute() {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected Bitmap doInBackground(Void... params) {
+
+            for (int i = 1; i <11 ; i++) {
+                sleep();
+                publishProgress(i*10);
+            }
+
+            Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher);
+            return bitmap;
+        }
+
+        private void sleep() {
             try {
-                Thread.sleep(4000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher);
-            return bitmap;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressBar.setProgress(values[0]);
         }
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
             mImageView.setImageBitmap(bitmap);
+            mProgressBar.setVisibility(View.INVISIBLE);
         }
     }
 
